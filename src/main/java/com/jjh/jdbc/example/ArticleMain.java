b70c12c554d8273851fb25510b029090a19e723f
정재환 <hwan5417@naver.com>
2019-03-29 14:55:43 +0900

    first commit

---
+++ src/main/java/com/jjh/jdbc/example/ArticleMain.java
@@ -0,0 +1,19 @@
+package com.jjh.jdbc.example;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ArticleMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("chap02.xml");
+		ArticleService articleService = ctx.getBean("articleService", com.jjh.jdbc.example.ArticleService.class);
+		MemberService memberService = ctx.getBean("memberService", com.jjh.jdbc.example.MemberService.class);
+
+		articleService.addArticle();
+		memberService.registerMember();
+
+		ctx.close();
+	}
+
+}
