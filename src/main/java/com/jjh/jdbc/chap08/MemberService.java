406bf3869c42e802d363fe88ba20a0994450c544
정재환 <hwan5417@naver.com>
2019-04-12 14:27:51 +0900

    ...

--- src/main/java/com/jjh/jdbc/chap08/MemberService.java
+++ src/main/java/com/jjh/jdbc/chap08/MemberService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
 import com.jjh.jdbc.chap3.Member;
 
 /**
@@ -11,6 +13,7 @@ import com.jjh.jdbc.chap3.Member;
  * 
  * @author Jacob
  */
+@Service
 public class MemberService {
 
 	Logger logger = LogManager.getLogger();
